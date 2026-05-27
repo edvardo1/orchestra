1626d4b7e3b9623ad3b0549d76306ddcc8411a55
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-05-27 16:09:23 -0300

    Added script to test BFS quickly

---
+++ run_BFS.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the user provided N
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <N>"
+    exit 1
+fi
+
+N=$1
+
+# Validate if N is a positive integer
+if ! [[ "$N" =~ ^[0-9]+$ ]]; then
+    echo "Error: N must be a positive integer."
+    exit 1
+fi
+
+# Execute the command N times - No GPU
+echo "Running BFS benchmark WITHOUT GPU acceleration..."
+
+for ((i=1; i<=N; i++)); do
+    mix run benchmarks/cooperative/bfs/BFS.exs \
+        benchmarks/cooperative/bfs/NYR_input.dat \
+        4096
+done
+
+# Execute the command N times - With GPU
+echo "Running BFS benchmark USING GPU acceleration..."
+
+
+for ((i=1; i<=N; i++)); do
+    mix run benchmarks/cooperative/bfs/BFS.exs \
+        benchmarks/cooperative/bfs/NYR_input.dat \
+        1024
+done
