299abc65c5f1cb2426c5f296b8f841b25351c3f3
edvardo1 <eduardobeloni@mailfence.com>
2026-05-28 11:16:55 -0300

    update to Orchestra, fix bugs in kernel

--- benchmarks/cooperative/bs/bs.ex
+++ benchmarks/cooperative/bs/bs.ex
@@ -1,11 +1,12 @@
-require OCLPolyHok
+require Orchestra
 
-OCLPolyHok.defmodule BezierSurface do
+Orchestra.defmodule BezierSurface do
   defd bezier_blend_gpu(k, mu, n) do
     nn = 0
     kn = 0
     nkn = 0
-    blend = 1
+
+    blend = 1.0
     nn  = n
     kn  = k
     nkn = n - k
@@ -14,11 +15,11 @@ OCLPolyHok.defmodule BezierSurface do
       blend = blend * nn
       nn = nn - 1
       if kn > 1 do
-        blend = blend / kn
+        blend = blend / (kn * 1.0)
         kn = kn - 1
       end
       if nkn > 1 do
-        blend = blend / nkn
+        blend = blend / (nkn * 1.0)
         nkn = nkn - 1
       end
     end
@@ -40,68 +41,60 @@ OCLPolyHok.defmodule BezierSurface do
     #xyz_l_in,
     xyz_in, xyz_outp
   ) do
-    p[5]
+    # p[0] = p.n_tasks
+    # p[1] = p.cut
+    # p[2] = p.current
+    p[3]
     p[0] = n_tasks # p.n_tasks = n_tasks
     if alpha >= 0.0 && alpha <= 1.0 do
       p[1] = p[0] * alpha # p.cut = p.n_tasks * alpha
     end # no else!!!
 
-    wg_in_J = (out_size_j - 1) / (get_local_size(0) + 1);
-    wg_in_I = (out_size_i - 1) / (get_local_size(1) + 1);
-
-    i = get_local_id(1) * get_local_size(0) + get_local_id(0)
-    #while i < (in_size_i + 1) * (in_size_j + 1) do
-    #  xyz_l_in[i] = xyz_in[i];
-    #  i = i + get_local_size(0) * get_local_size(1)
-    #end
-
-    __syncthreads() # barrier(CLK_LOCAL_MEM_FENCE)
+    wg_in_J = (out_size_j - 1) / get_local_size(0) + 1;
+    wg_in_I = (out_size_i - 1) / get_local_size(1) + 1;
 
     p[2] = p[1] + get_group_id(0) # p.current = p.cut + get_group_id(0)
     t = p[2]
     while p[2] < p[0] do # p.current < p.n_tasts
       my_s1 = t / wg_in_J
       my_s0 = t
-      while my_s0 > wg_in_J do
+      while my_s0 >= wg_in_J do
         my_s0 = my_s0 - wg_in_J # hack for %
       end
 
-      #row = my_s1 * get_local_size(1) + get_local_id(1)
-      #col = my_s0 * get_local_size(0) + get_local_id(0)
       row = my_s1 * get_local_size(1) + get_local_id(1)
       col = my_s0 * get_local_size(0) + get_local_id(0)
-      bi = 0
-      bj = 0
-      mui = row / (out_size_i - 1)
-      muj = row / (out_size_j - 1)
+      bi = 0.0
+      bj = 0.0
+      mui = 1.0 * (row / (1.0 * (out_size_i - 1)))
+      muj = 1.0 * (col / (1.0 * (out_size_j - 1)))
 
       if row < out_size_i && col < out_size_j do
         out[3]
-        out[0] = 0
-        out[1] = 0
-        out[2] = 0
+        out[0] = 0.0
+        out[1] = 0.0
+        out[2] = 0.0
 
         ki = 0
         while ki <= in_size_i do
           bi = bezier_blend_gpu(ki, mui, in_size_i)
 
           kj = 0
-          while kj <= in_size_i do
-            bi = bezier_blend_gpu(kj, muj, in_size_j)
+          while kj <= in_size_j do
+            bj = bezier_blend_gpu(kj, muj, in_size_j)
 
-            out[0] = xyz_in[(ki * (in_size_j + 1) + kj) * 3 + 0] * bi * bj
-            out[1] = xyz_in[(ki * (in_size_j + 1) + kj) * 3 + 1] * bi * bj
-            out[2] = xyz_in[(ki * (in_size_j + 1) + kj) * 3 + 2] * bi * bj
-
+            out[0] = out[0] + (xyz_in[(ki * (in_size_j + 1) + kj) * 3 + 0] * bi * bj)
+            out[1] = out[1] + (xyz_in[(ki * (in_size_j + 1) + kj) * 3 + 1] * bi * bj)
+            out[2] = out[2] + (xyz_in[(ki * (in_size_j + 1) + kj) * 3 + 2] * bi * bj)
             kj = kj + 1
           end
 
           ki = ki + 1
         end
 
-        xyz_outp[(row * out_size_j + col) * 3 + 0] = out[0]
-        xyz_outp[(row * out_size_j + col) * 3 + 1] = out[1]
-        xyz_outp[(row * out_size_j + col) * 3 + 2] = out[2]
+        xyz_outp[3 * (row * out_size_j + col) + 0] = out[0]
+        xyz_outp[3 * (row * out_size_j + col) + 1] = out[1]
+        xyz_outp[3 * (row * out_size_j + col) + 2] = out[2]
       end
 
       p[2] = p[2] + get_num_groups(0) # p.current = p.current + get_num_groups(0)
@@ -123,7 +116,7 @@ OCLPolyHok.defmodule BezierSurface do
     in_len   = (in_size_i + 1) * (in_size_j + 1) * 3
     out_len  = out_size_i * out_size_j * 3
 
-    gpu_ctx = OCLPolyHok.gpu()
+    gpu_ctx = Orchestra.gpu()
 
     tensor =
       "benchmarks/cooperative/bs/control.txt"
@@ -135,36 +128,10 @@ OCLPolyHok.defmodule BezierSurface do
       |> Nx.tensor()
       |> Nx.reshape({16 * 3})
 
-    IO.write("n_work_items: ")
-    IO.inspect(n_work_items)
-    IO.write("n_work_groups: ")
-    IO.inspect(n_work_groups)
-    IO.write("n_tasks: ")
-    IO.inspect(n_tasks)
-    IO.write("alpha: ")
-    IO.inspect(alpha)
-    IO.write("in_size_i: ")
-    IO.inspect(in_size_i)
-    IO.write("in_size_j: ")
-    IO.inspect(in_size_j)
-    IO.write("out_size_i: ")
-    IO.inspect(out_size_i)
-    IO.write("out_size_j: ")
-    IO.inspect(out_size_j)
+    in_gnx = Orchestra.new_gnx(Orchestra.gpu(), tensor)
+    out_gnx = Orchestra.new_gnx(gpu_ctx, {out_len}, {:f, 32})
 
-    in_size = in_len * 4
-    out_size = out_len * 4
-    IO.write("in_size: ")
-    IO.inspect(in_size)
-    IO.write("out_size: ")
-    IO.inspect(out_size)
-
-    IO.inspect(Nx.reshape(tensor, {16, 3}))
-
-    in_gnx = OCLPolyHok.new_gnx(OCLPolyHok.gpu(), tensor)
-    out_gnx = OCLPolyHok.new_gnx(gpu_ctx, {out_len}, {:f, 32})
-
-    OCLPolyHok.spawn(
+    Orchestra.spawn(
       gpu_ctx,
       &BezierSurface.bezier_surface/8,
 
@@ -175,8 +142,6 @@ OCLPolyHok.defmodule BezierSurface do
        in_size_i, in_size_j, out_size_i, out_size_j,
        in_gnx, out_gnx]
     )
-
-    IO.inspect(Nx.to_binary(OCLPolyHok.get_gnx(gpu_ctx, out_gnx)), limit: 128)
   end
 end
 
